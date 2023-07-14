4349b04dbdce261935c7223172fe72897d0eb6a2
Luis Martinez <mr.luiswebmaster@gmail.com>
2023-07-13 20:31:55 -0500

    feat: :sparkles: new command for creating stickers with text

--- src/whatsapp/whatsapp.service.js
+++ src/whatsapp/whatsapp.service.js
@@ -3,9 +3,12 @@ const fs = require("fs");
 const mime = require("mime-types");
 const path = require("path");
 const qrcode = require("qrcode-terminal");
-const getStickersbyServiceGif = require("../services/gif");
+const { createCanvas, registerFont } = require('canvas');
 
+const getStickersbyServiceGif = require("../services/gif");
 const { COMMANDS, INFO_MESSAGES } = require("../utils/messages");
+const splitTextIntoLines   = require("../utils/nextLine");
+const { colors }   = require("../utils/colors");
 
 const allowedTypes = ["image", "video"];
 
@@ -49,7 +52,6 @@ class whatsappService {
 
         const extension = mime.extension(media.mimetype);
         const filename = new Date().getTime();
-
         const fullFilename = path.join(mediaPath, filename + "." + extension);
 
         fs.writeFileSync(fullFilename, media.data, { encoding: "base64" });
@@ -75,7 +77,6 @@ class whatsappService {
       }
     } catch (err) {
       console.log("Failed to save the file:", err);
-      fs.unlinkSync(fullFilename);
       console.log(`File deleted successfully!`);
       await this.client.sendMessage(message.from, INFO_MESSAGES.ERROR_STICKER);
     }
@@ -119,6 +120,79 @@ class whatsappService {
     }
   }
 
+  async createStickerByText(message, options) {
+    try {
+
+      await this.client.sendMessage(
+        message.from,
+        INFO_MESSAGES.CREATING_STICKER
+      );
+
+
+      function createCenteredTextImage(text, width = 1200, height = 1200, bgcolor = colors.black, textcolor = colors.white ) {
+        const canvas = createCanvas(width, height);
+        const context = canvas.getContext('2d');
+
+        
+        const fontSize = Math.floor(Math.min(width, height) * 0.1);
+        context.font = `${fontSize}px Arial`;
+
+
+        context.fillStyle = bgcolor || colors[bgcolor]; 
+
+        context.fillRect(0, 0, width, height);
+        const lines = splitTextIntoLines(text, width - 25, context);
+
+        context.fillStyle = textcolor || colors[textcolor]; 
+
+        const textHeight = lines.length * fontSize * 1.2;
+
+        const textY = (height - textHeight) / 2 + fontSize + 25;
+
+        lines.forEach((line, index) => {
+          const textWidth = context.measureText(line).width;
+          const textX = (width - textWidth) / 2 + 25;
+          const lineY = textY + index * fontSize * 1.2;
+          context.fillText(line, textX, lineY);
+        });
+        return canvas.toBuffer("image/png");
+      }
+
+      let buffer = null
+
+      if (typeof options === 'string') {
+        buffer = createCenteredTextImage(options, 1200, 1200);
+      } else {
+        const {text, bgcolor, textcolor} = options
+        buffer = createCenteredTextImage(text, 900, 900, bgcolor, textcolor);
+      }
+
+      const mediaPath = path.join(__dirname, "../../public/uploads");
+      const extension = 'png';
+      const filename = new Date().getTime();
+      const fullFilename = path.join(mediaPath, filename + "." + extension);
+
+      fs.writeFileSync(fullFilename, buffer, { encoding: "base64" });
+
+      await this.client.sendMessage(
+        message.from,
+        MessageMedia.fromFilePath(fullFilename),
+        {
+          sendMediaAsSticker: true,
+          stickerAuthor: "Created by bot and made by Luis Martinez",
+          stickerName: "Stickers",
+        }
+      );
+
+      fs.unlinkSync(fullFilename);
+
+      await this.client.sendMessage(message.from, INFO_MESSAGES.FINISH_STICKER);
+    } catch (err) {
+      console.log(err)
+      await this.client.sendMessage(message.from, INFO_MESSAGES.ERROR_STICKER);
+    }
+  }
+
   async dispatchServicesForBot(message) {
     let chat = await message.getChat();
     let messageBody = message.body;
@@ -136,6 +210,45 @@ class whatsappService {
         this.createStickerByGif(message, messageForFunky);
       } else {
         message.reply(COMMANDS.FUNKY.messages.INVALID_COMMAND);
+      }
+    } else if (messageBody.startsWith(COMMANDS.CREATE.type)) {
+      let messageForCreate = messageBody
+        .slice(messageBody.indexOf(":") + 1)
+        .trimStart();
+        if (messageForCreate) {
+            try {
+              const parenthesisStartIndex = messageForCreate.indexOf("(");
+              const parenthesisEndIndex = messageForCreate.indexOf(")");
+            
+              if (parenthesisStartIndex !== -1 && parenthesisEndIndex !== -1 && parenthesisStartIndex < parenthesisEndIndex) {
+                const result = messageForCreate.slice(parenthesisStartIndex + 1, parenthesisEndIndex).trim();
+                const remainingPart = messageForCreate.slice(0, parenthesisStartIndex).trim();
+            
+                const values = result.split(",").map(value => value.split(":"));
+                const options = {
+                  text: remainingPart
+                };
+            
+                values.forEach(([property, val]) => {
+                  const propertyTrimmed = property.trim();
+                  const valueTrimmed = val.trim();
+            
+                  if (propertyTrimmed === "bgcolor" && !options.bgcolor) {
+                    options.bgcolor = valueTrimmed;
+                  } else if (propertyTrimmed === "textcolor" && !options.textcolor) {
+                    options.textcolor = valueTrimmed;
+                  }
+                });
+                this.createStickerByText(message, options);
+              } else {
+                this.createStickerByText(message, messageForCreate);
+              }
+            } catch (error) {
+              await this.client.sendMessage(message.from, INFO_MESSAGES.ERROR_STICKER);
+            }
+        }
+         else {
+        message.reply(COMMANDS.CREATE.messages.INVALID_COMMAND);
       }
     } else if (messageBody.trim() === COMMANDS.HELP.type) {
       message.reply(COMMANDS.HELP.getMenuMessage());
