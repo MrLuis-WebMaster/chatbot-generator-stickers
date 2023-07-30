cd76fa52b5597193dabfb840e4704cfdc2be3c2e
Luis Martinez <mr.luiswebmaster@gmail.com>
2023-07-29 19:58:22 -0500

    fix: :bug: remove cronjob

--- src/services/cronjobs.service.js
+++ src/services/cronjobs.service.js
@@ -1,11 +1,7 @@
-const cron = require("node-cron");
 const MailService = require("./mail.service");
 
 const mailService = new MailService();
 
-cron.schedule("0 9 * * 1", async () => {
+(async () => {
   await mailService.sendEmailReport();
-}, {
-  scheduled: true,
-  timezone: "America/Bogota"
-});
+})();
