7b82ad0197306cf0a254f8311ac5104617bc3878
James Harrison <james@fl0.com>
2023-08-25 17:33:33 -0700

    Dockerfile working

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'src/views'));
+app.set('views', path.join(__dirname, 'views'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
